95ca2829b48a0d9e30cfdbe3f7ddedbdc03d9d9d
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-09-23 15:05:48 +0600

    add cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,29 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = (props) => {
-    let total= 0;
-    for(const product of props.products){
-        total = total + product.price;
-    }
+  const product = props.products;
+  const total = product.reduce(
+    (previous, current) => previous + current.price,
+    0
+  );
+  const shiping = product.reduce(
+    (previous, current) => previous + current.shipping,
+    0
+  );
+  const tax = product.reduce(
+    (previous, current) => previous + current.price / 10,
+    0
+  );
+  const grandTotal = total + shiping + tax;
+
   return (
     <div>
       <h1>cart</h1>
       <h3>items orderd:{props.products.length}</h3>
-      <h3>Order Total:{total}</h3>
+      <h3>Order Total:{total.toFixed(2)}</h3>
+      <h3>Shiping:{shiping.toFixed(2)}</h3>
+      <h3>Tax:{tax.toFixed(2)}</h3>
+      <h3>Grandtotal:{grandTotal.toFixed(2)}</h3>
     </div>
   );
 };
